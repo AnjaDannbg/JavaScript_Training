2660f406df411214558ea09da3fe2b3c7fb48344
Anja Dannenberg <anja.dannbg@gmail.com>
2025-02-04 14:29:50 +0100

    Uebung2_13 fertig

--- Woche 4/Tag_16_Montag_2025_02_03/uebung2_13.js
+++ Woche 4/Tag_16_Montag_2025_02_03/uebung2_13.js
@@ -2,8 +2,28 @@
 
 'use strict';
 
-/* your code here */
+// triangle descending
+const triangle = (size) => {
+    if(size > 0) {
+        return `${line(size, '*')}\n${triangle(size - 1)}`;
+    }
+    return '';
+}
+
+// triangle ascending
+const triangleAscending = (size) => {
+    return buildTriangle(size, 0);
+}
+
+const buildTriangle = (size, index) => {
+    if(size >= 0) {
+        return `${line(index, '*')}\n${buildTriangle(size - 1, index + 1)}`;
+    }
+    return '';
+}
+
+
 const line = (length, character) =>
     length === 0 ? '' : character + line(length - 1, character);
 
-console.log(triangle(10));
+console.log(triangleAscending(10));
