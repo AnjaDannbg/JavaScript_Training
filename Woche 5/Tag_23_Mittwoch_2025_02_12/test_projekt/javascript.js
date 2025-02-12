50d331ece2197acc331e6557c470dca1b3b3e379
Anja Dannenberg <anja.dannbg@gmail.com>
2025-02-12 23:28:25 +0100

    Woche 5 Uebungen und Tests

---
+++ Woche 5/Tag_23_Mittwoch_2025_02_12/test_projekt/javascript.js
@@ -0,0 +1,22 @@
+'use strict';
+const $ = (qs) => document.querySelector(qs);
+const $$ = (qs) => Array.from(document.querySelectorAll(qs));
+
+document.querySelector('p').innerHTML = "Abend!";
+let myNodeList = document.querySelectorAll('p');
+
+myNodeList[0].innerHTML = "0";
+myNodeList[1].innerHTML = "1";
+myNodeList[2].innerHTML = "2";
+
+for(let i=0; i<myNodeList.length; i++) {
+  myNodeList[i].innerHTML = 10 + i;
+}
+
+$('p').innerHTML = "§§§§§"
+
+$$("p").map((p, i) => {
+  if(i===2)
+    return p.innerHTML = "---------"
+  }
+);
